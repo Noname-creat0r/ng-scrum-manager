2ca7ef26dc19e5d710d65e7212020471436299a1
Noname-creat0r <egorchik228322@gmail.com>
2023-04-25 22:50:14 +0300

    refactor(styles): leaning base toolbar classes

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import {CommonModule, NgOptimizedImage} from '@angular/common';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: 'header.template.html',
-  imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule],
 })
 export class HeaderComponent {
   username: string = 'Username';
