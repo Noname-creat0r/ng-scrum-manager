384d4618a34712d2ca68db3f4cf873199cd489b9
Noname-creat0r <egorchik228322@gmail.com>
2023-04-25 22:51:02 +0300

    feat(components): creating FooterComponent + Styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent} from "./layout/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html' ,
     styleUrls: ['../styles.scss'],
     standalone: true,
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'scrum-manager';
---
+++ src/app/layout/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  templateUrl: 'footer.component.html',
+  imports: [CommonModule],
+})
+export class FooterComponent {
+
+}
