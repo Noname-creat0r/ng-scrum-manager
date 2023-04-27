ffb065986b8b406f4338d725ffbe21551fddb71e
Noname-creat0r <egorchik228322@gmail.com>
2023-04-27 23:39:34 +0300

    feat(components): creating Modal component

---
+++ src/app/shared/components/modal/modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: 'modal.component.html'
+})
+export class ModalComponent {
+
+}
