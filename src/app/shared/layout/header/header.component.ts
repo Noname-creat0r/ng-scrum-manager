57d841eb2a3179cc3f707d982e53b82dce64a9f3
Noname-creat0r <egorchik228322@gmail.com>
2023-04-27 23:38:58 +0300

    refactor(components): implementing DRYed styles and small changes

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
+
+import { ModalComponent } from "../../components/modal/modal.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: 'header.template.html',
-  imports: [CommonModule],
+  imports: [CommonModule, ModalComponent],
 })
 export class HeaderComponent {
-  username: string = 'Username';
+  username: string | null = null;
+  unauth: boolean = false;
+  showAuthModal: boolean = false;
+
+  onShowAuthModal() {
+    this.showAuthModal = !this.showAuthModal;
+  }
 }
