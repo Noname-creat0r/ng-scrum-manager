cccccf65994ed7c929d068efd7a8e261bfb56adc
Noname-creat0r <egorchik228322@gmail.com>
2023-05-11 10:36:46 +0300

    feat(components): adding Logout modal context

--- src/app/auth/logout/logout.component.ts
+++ src/app/auth/logout/logout.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { SignInActions } from '../store/auth.actions';
 
 @Component({
   selector: 'app-logout',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      logout works!
-    </p>
-  `,
+  templateUrl: 'logout.component.html',
 })
 export class LogoutComponent {
 
+  constructor(public activeModal: NgbActiveModal, private readonly store: Store) { }
+
+  onLogout() {
+     this.store.dispatch(SignInActions.logout())
+     this.activeModal.close();
+  }
 }
