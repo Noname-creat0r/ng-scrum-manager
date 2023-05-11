60bb6463c36a3695beb1eee496e074f8532b6b3e
Noname-creat0r <egorchik228322@gmail.com>
2023-05-11 10:38:05 +0300

    feat(components): calling Logout modal func + auth status options on toolbar

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,30 +1,38 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { CommonModule, NgIf } from '@angular/common';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Store } from '@ngrx/store';
+
 import { SignInComponent } from '../../../auth/index';
-import { slideYAnimation } from '../../animations/toolbar-slide.animation';
+import { LogoutComponent } from 'src/app/auth/logout/logout.component';
+import { selectIsAuthenticated } from 'src/app/auth/store/auth.reducer';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: 'header.template.html',
-  imports: [CommonModule],
-  animations: [slideYAnimation]
+  imports: [CommonModule, NgIf],
 })
 export class HeaderComponent {
   username: string | null = null;
-  unauth: boolean = false;
   state: string = 'shown';
+  isAuth$ = this.store.select(selectIsAuthenticated);
 
-  constructor(private modalService: NgbModal) { }
+  constructor(
+    private modalService: NgbModal,
+    private readonly store: Store) {
+  }
 
   onToggleToolbar() {
-    this.state = this.state === 'shown' ? 'hidden' : 'shown'
+    this.state = this.state === 'shown' ? 'hidden' : 'shown';
   }
 
   onShowSignInModal() {
-    this.modalService.open(SignInComponent, { size: 'sm' })
+    this.modalService.open(SignInComponent, { size: 'sm', backdrop: 'static' });
+  }
+
+  onShowLogoutDialog() {
+    this.modalService.open(LogoutComponent, { size: 'sm' });
   }
 
 }
