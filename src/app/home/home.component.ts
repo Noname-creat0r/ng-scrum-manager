173a871088dcaecba0f55cf6d346e841fc5a8e02
Noname-creat0r <egorchik228322@gmail.com>
2023-05-13 21:17:17 +0300

    feat(components): creating Home componet for projects overreview

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,5 +8,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  
+  projects = [
+    {
+      title: 'hellnaw',
+      description: 'lol',
+      id: '#ad12dad',
+      tags: [ { type: 'bg-warning', text: 'important' }, { type: 'bg-primary', text: 'main'} ]
+    },
+    {
+      title: 'hellnaw',
+      description: 'lol',
+      id: '#ad12dad',
+      tags: []
+    },
+    {
+      title: 'hellnaw',
+      description: 'Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatatdawdawdawddddddddddddddddddddddddddddddddddddddddddddddddddddddd adw.',
+      id: '#ad12dad',
+      tags: []
+    }
+  ] 
 }
