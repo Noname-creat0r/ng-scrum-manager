81583172967b80e024bab128eaf685ce5b5727fc
Noname-creat0r <egorchik228322@gmail.com>
2023-05-18 01:31:31 +0300

    build(npm): updating @ng-bootstrap and removing index files

--- src/app/auth/index.ts
+++
@@ -1,3 +0,0 @@
-export { AUTH_ROUTES, authPaths } from "./auth.routes";
-export * from "./signin/signin.component";
-export * from "./signup/signup.component";
--- src/app/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./app.component"
-export {appPaths, appRoutes} from "./app.routes"
