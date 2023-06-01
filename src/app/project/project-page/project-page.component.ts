3da7beba85bb2f25e499262ed238488861e85446
Noname-creat0r <egorchik228322@gmail.com>
2023-06-01 15:55:39 +0300

    feat(components): displaying current project data on project-page

--- src/app/project/project-page/project-page.component.ts
+++ src/app/project/project-page/project-page.component.ts
@@ -1,19 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { DragDropModule, CdkDragDrop, moveItemInArray, transferArrayItem, } from '@angular/cdk/drag-drop';
 
 import { ProjectModel } from '../project.model';
+import { selectProject, selectCurrentProjectId } from '../store/project.reducer';
+import { Observable, Subject, Subscription } from 'rxjs';
 
+import { LoadingProjectsActions } from '../store/project.actions';
 @Component({
   selector: 'project-page',
   standalone: true,
   imports: [CommonModule, DragDropModule, NgFor, NgIf],
   templateUrl: 'project-page.component.html'
 })
-export class ProjectPageComponent {
-  @Input() project!: ProjectModel
-
+export class ProjectPageComponent implements OnInit, OnDestroy {
+  project$!: Observable<ProjectModel | undefined> 
+  
   boardContainers = [
     { 
       title: 'Todo',
@@ -40,6 +44,21 @@ export class ProjectPageComponent {
     }
   ]
   
+  constructor(
+    private readonly store: Store,
+    private router: Router,
+    private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.store.dispatch(LoadingProjectsActions.initialized({ userId: undefined }))
+    this.project$ = this.store.select(selectProject())
+ 
+  }
+  
+  ngOnDestroy() {
+  }
+
+
   drop(event: CdkDragDrop<Array<{content: string, disabled?: boolean}>>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex)
@@ -53,4 +72,4 @@ export class ProjectPageComponent {
   // get current project here through selector
   // load iterations + backlog tasks
 
-}
+} 
