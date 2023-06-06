73a42969b53640d23c21c3be54d7d2a6cac44be5
Noname-creat0r <egorchik228322@gmail.com>
2023-06-07 01:50:57 +0300

    feat(task): selecting task on task options click and triggering task-control modal open for edit

--- src/app/task/task-board/task-list/task-item/task-item.component.ts
+++ src/app/task/task-board/task-list/task-item/task-item.component.ts
@@ -1,16 +1,37 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule, NgIf } from '@angular/common';
-import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { Store } from '@ngrx/store';
 
 import { TaskModel } from '../../../task.model';
+import { TaskActions } from 'src/app/task/store/task.actions';
+import { TaskControlComponent } from 'src/app/task/task-control/task-control.component';
 
 @Component({
   selector: 'task-item',
   standalone: true,
-  imports: [CommonModule, NgbPopoverModule, NgIf],
+  imports: [CommonModule, NgbPopoverModule],
   templateUrl: 'task-item.component.html'
 })
 export class TaskItemComponent {
   @Input() task!: TaskModel
+
+  constructor(
+    private readonly store: Store,
+    private modalService: NgbModal) {}
+
+  onPopoverShown() {
+    this.store.dispatch(TaskActions.selected({ taskId: this.task.id?.toString() }))
+  }
+
+  onPopoverHidden() {}
+
+  onTaskEditClicked() {
+   this.modalService.open(TaskControlComponent) 
+  }
+
+  onTaskDeleteClicked() {
+
+  }
 }
