757477bd18e4e9901961153e9898178a392a359b
Noname-creat0r <egorchik228322@gmail.com>
2023-06-09 20:32:19 +0300

    fix(task): loading and handling current task info immutably

--- src/app/task/task-control/task-control.component.ts
+++ src/app/task/task-control/task-control.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Store } from '@ngrx/store';
+import { Observable, Subscription, window } from 'rxjs';
 
 import { AddingTaskActions, EditingTaskActions } from '../store/task.actions';
 import { TaskModel } from '../task.model';
 import { selectCurrentProjectId } from 'src/app/project/store/project.reducer';
-import { selectTaskId } from '../store/task.reducer';
+import { selectTask, selectTaskId } from '../store/task.reducer';
 
 @Component({
   selector: 'app-task-control',
@@ -15,10 +16,15 @@ import { selectTaskId } from '../store/task.reducer';
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: 'task-control.component.html' 
 })
-export class TaskControlComponent {
+export class TaskControlComponent implements OnInit, OnDestroy {
   taskForm!: FormGroup;
  
-  // load task here and set the default data
+  private taskIdSub!: Subscription
+  private taskSub!: Subscription
+  private projectIdSub!: Subscription
+
+  //editTask!: TaskModel
+  public mode: "Add" | "Edit" = "Add"
   public taskId: string | undefined = undefined
   public projectId: string | undefined = undefined  
 
@@ -26,41 +32,61 @@ export class TaskControlComponent {
     public activeModal: NgbActiveModal,
     private readonly store: Store) {}
 
-  ngOnInit() {
-    this.taskForm = new FormGroup({ 
-       'title': new FormControl(null, [Validators.required]),
-       'description': new FormControl(null, [
-          Validators.required, 
-          Validators.minLength(1),
-          Validators.maxLength(512)
-       ]),
-      'status': new FormControl(null, [Validators.required ]),
-      'storyPoints': new FormControl(null, [Validators.required ])
-    });
+  createTaskForm(task?: TaskModel) {
+    return new FormGroup({ 
+      'title': new FormControl(task?.title || null, [Validators.required]),
+      'description': new FormControl(task?.description || null, [
+        Validators.required, 
+        Validators.minLength(1),
+        Validators.maxLength(512)
+      ]),
+      'status': new FormControl(task?.status.status || null, [Validators.required ]),
+      'storyPoints': new FormControl(task?.storyPoints || null, [Validators.required ])
+    }); 
+  }
 
-    this.store.select(selectCurrentProjectId).subscribe(id => {
+  ngOnInit() {
+    this.projectIdSub = this.store.select(selectCurrentProjectId).subscribe(id => {
       this.projectId = id
     })
 
-    this.store.select(selectTaskId).subscribe(id => {
+    this.taskIdSub = this.store.select(selectTaskId).subscribe(id => {
       this.taskId = id
     })
 
+    this.taskSub = this.store.select(selectTask()).subscribe(task => {
+      if (task) {
+        this.mode = "Edit"
+        //this.editTask = task
+        this.taskForm = this.createTaskForm(task) 
+      } else {
+        this.mode = "Add"
+        this.taskForm = this.createTaskForm()
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.projectIdSub.unsubscribe()
+    this.taskIdSub.unsubscribe()
+    this.taskSub.unsubscribe()
   }
 
   onSubmit() {
     const task: TaskModel = {
+      id: this.taskId ? +this.taskId : 0,
       title: this.taskForm.get('title')?.value,
       description: this.taskForm.get('description')?.value,
       storyPoints: this.taskForm.get('storyPoints')?.value,
       projectId: this.projectId ? +this.projectId : undefined,
+      bContainerPos: 0,
       status: this.taskForm.get('status')?.value
     }
     
     if (!this.taskId) {
       this.store.dispatch(AddingTaskActions.initialized({ task: task }))
     } else {
-      this.store.dispatch(EditingTaskActions.initialized({ task: task }))
+      this.store.dispatch(EditingTaskActions.initialized({ task: task}))
     }
   }
 
